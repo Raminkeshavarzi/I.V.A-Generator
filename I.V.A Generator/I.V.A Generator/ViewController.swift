d880550ce3c6d9cd0ffcdffe16996c7d311c5f08
Ramin Keshavarzi <you@example.com>
2021-03-06 17:50:33 +0100

    installed the app

---
+++ I.V.A Generator/I.V.A Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I.V.A Generator
+//
+//  Created by Ramin on 6/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
